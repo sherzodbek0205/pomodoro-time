89f99ff99b3082ef8c84620a01da611003e148ed
Sherzodbek0205 <sherzodbekusmonov178@gmail.com>
2023-09-24 18:36:35 +0500

    timer finished

--- src/index.js
+++ src/index.js
@@ -4,47 +4,61 @@ const long = document.querySelector(".long");
 const main = document.querySelector(".main");
 const board = document.querySelector(".board");
 const start = document.querySelector(".start");
+const title = document.querySelector("title");
+const pause = document.querySelector(".pause");
+const timerElement = document.getElementById("timer");
+
+let countdownTime = 300;
+let timerInterval;
+
 short.onclick = () => {
-main.classList = "next";
-board.classList = "board one"
-countdownTime = 3;
-}
+  timerElement.innerText = "01:00";
+  main.classList = "next";
+  board.classList = "board one";
+  countdownTime = 180;
+};
+
 pomodoro.onclick = () => {
+  timerElement.innerText = "05:00";
   main.classList = "";
   board.classList = "board";
-}
+};
+
 long.onclick = () => {
+  timerElement.innerText = "15:00";
   main.classList = "next2";
   board.classList = "board two";
-}
- let countdownTime = 300;
+  countdownTime = 600;
+};
 
-
- let timerElement = document.getElementById("timer");
 start.onclick = () => {
- start.innerText = "Pause";
+  start.style.display = "none";
+  pause.style.display = "block";
 
+  timerInterval = setInterval(updateTimer, 1000);
+};
 
- function updateTimer() {
-   let minutes = Math.floor(countdownTime / 60);
-   let seconds = countdownTime % 60;
+pause.onclick = () => {
+  start.style.display = "block";
+  pause.style.display = "none";
 
-   let formattedTime = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+  clearInterval(timerInterval);
+};
 
+function updateTimer() {
+  let minutes = Math.floor(countdownTime / 60);
+  let seconds = countdownTime % 60;
 
-   timerElement.textContent = formattedTime;
+  let formattedTime = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
 
+  timerElement.textContent = formattedTime;
+  title.innerText = formattedTime;
 
-   countdownTime--;
+  countdownTime--;
 
-
-   if (countdownTime < 0) {
-     clearInterval(timerInterval);
-     timerElement.style.fontSize = "85px"
-     timerElement.textContent = "Time's Up";
-
-   }
- }
-
- let timerInterval = setInterval(updateTimer, 1000);
+  if (countdownTime < 0) {
+    clearInterval(timerInterval);
+    timerElement.style.fontSize = "85px";
+    timerElement.textContent = "Time's Up";
+  }
 }
